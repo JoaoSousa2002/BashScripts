50822b190c42b4edb52a34492151b27d4af1ac42
JoaoSousa2002 <joao21senac@gmail.com>
2026-03-15 22:16:02 -0300

    first commit

---
+++ CriarArquivoCode.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+cd /home/joao/Desktop/'Estudos faculdade 02.2026'
+
+# Cria e retorna pasta chamada ListaExercicioXX/ ou retorna a pasta Estudos/
+function CriarPastaOuNao {
+    if echo $1 | grep -q Exercicio ; then
+        declare -i QuantidadePastas=$(ls | grep -c Exercicio)  #Quantidade de pastas ja existentes com a palavra Exercicio
+        if [ $QuantidadePastas -lt 10 ] ;then
+            QuantidadePastas+=1
+            mkdir ListaExercicios0$QuantidadePastas
+            echo ListaExercicios0$QuantidadePastas/
+        else
+            mkdir ListaExercicios$QuantidadePastas
+            echo ListaExercicios$QuantidadePastas/
+        fi 
+    else
+        #Verifica se a pasta não existe, se não existir, cria a pasta Estudos
+        if [ ! -d "Estudos" ]; then
+            mkdir Estudos
+        fi
+        echo Estudos/
+    fi
+}
+
+
+echo "================================================================================"
+echo "@              *                          *                     *              @"
+echo "@                     CRIAR ARQUIVO E ABRIR NO VSCODE                          @"
+echo "@       *                *          *                     *                    @"
+echo "================================================================================"
+
+echo ""
+read -p "Digite o nome do arquivo para criar (incluindo a extensão): " Resposta
+
+# Verificação e criação para arquivo Python
+# Não foi criado em função pois em bash a função não cria arquivo permanente com touch
+if echo $Resposta | grep -q ".py"; then
+    cd Python/
+    CaminhoPasta=$(CriarPastaOuNao $Resposta)
+    echo $CaminhoPasta
+    cd $CaminhoPasta
+    touch $Resposta
+    code $Resposta
+
+# Verificação e criação para arquivo JavaScript
+elif echo $Resposta | grep -q ".py" ;then
+    cd 'WEB I'/'Estudos JS'/Javascript/
+    CaminhoPasta=$(CriarPastaOuNao $Resposta)
+    cd $CaminhoPasta
+    touch $Resposta
+    code $Resposta
+
+else
+    echo ""
+    echo " XXX Extensão não reconhecida"
+    echo ""
+    read -p "Pressione Enter para fechar..."
+fi
+
+
+
+
+
